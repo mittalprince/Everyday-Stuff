5fff98c0ed131ba0670ddb2866043460ff2df550
mittalprince <princemittal766@gmail.com>
2018-04-05 23:08:51 +0530

    heelo world program

--- c++/c1(helloworld).cpp
+++ c++/c1(helloworld).cpp
@@ -1,10 +1,40 @@
 #include<iostream>
-
+#include<stack>
 using namespace std;
 
-int main(){
+int M(int a, int b){
+    if(a>b)
+        return a;
+    else
+        return b;
+}
 
-cout<<"Hello, World! ";
 
-return 0;
+int main() {
+    int n;
+    stack<int> s;
+    int max=0,ch,no,temp;
+    while(n--){
+        cin>>ch;
+        if(ch==1){
+            cin>>no;
+            if(s.empty()){
+                s.push(no);
+            }
+            else{
+                temp =M(no,s.top());
+                s.push(temp);
+            }
+        }
+        else if(ch==2){
+            if(!s.empty()){
+                s.pop();
+             }
+        }
+        else if(ch==3){
+            cout<<s.top();
+            cout<<endl;
+        }
+    }
+    return 0;
 }
