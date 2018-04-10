b8ade68a849f4008b7306a6b3f1d71ad74c52600
mittalprince <princemittal766@gmail.com>
2018-04-10 19:36:47 +0530

    Hello world program

--- c++/c1(helloworld).cpp
+++ c++/c1(helloworld).cpp
@@ -1,40 +1,10 @@
 #include<iostream>
-#include<stack>
+
 using namespace std;
 
-int M(int a, int b){
-    if(a>b)
-        return a;
-    else
-        return b;
-}
+int main(){
 
+cout<<"Hello, World! ";
 
-int main() {
-    int n;
-    stack<int> s;
-    int max=0,ch,no,temp;
-    while(n--){
-        cin>>ch;
-        if(ch==1){
-            cin>>no;
-            if(s.empty()){
-                s.push(no);
-            }
-            else{
-                temp =M(no,s.top());
-                s.push(temp);
-            }
-        }
-        else if(ch==2){
-            if(!s.empty()){
-                s.pop();
-             }
-        }
-        else if(ch==3){
-            cout<<s.top();
-            cout<<endl;
-        }
-    }
-    return 0;
+return 0;
 }
