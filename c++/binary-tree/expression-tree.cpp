5a0f70f87d01c9a9ac8f56b990cd847684518b09
mittalprince <princemittal766@gmail.com>
2018-06-28 23:29:55 +0530

    Given postfix expression construct BT

---
+++ c++/binary-tree/expression-tree.cpp
@@ -0,0 +1,76 @@
+#include<iostream>
+#include<sstream>
+#include<cctype>
+#include<stack>
+
+using namespace std;
+
+struct node{
+  char data;
+  node *left, *right;
+};
+
+struct node* newNode(char key){
+  struct node *Node = new node;
+  Node->data = key;
+  Node->left =NULL;
+  Node->right = NULL;
+
+  return Node;
+}
+
+bool isoperand(char c){
+  if(c == '+' || c == '-' ||
+            c == '*' || c == '/' ||
+            c == '^')
+            return true;
+      return false;
+}
+
+struct node* expression(string str){
+  stringstream s(str);
+  char c;
+  node *root;
+  stack<node* > S;
+  node *val1,*val2;
+  while(s >> c){
+
+  if(!isoperand(c)){
+    root = newNode(c);
+    S.push(root);
+  }
+  else{
+    root = newNode(c);
+    val1 = S.top();
+    S.pop();
+    val2 = S.top();
+    S.pop();
+    root->left = val2;
+    root->right = val1;
+    S.push(root);
+
+    }
+  }
+  root = S.top();
+  S.pop();
+  return root;
+  //inorderTraversal(root);
+}
+
+void inorderTraversal(struct node* root){
+    if(root==NULL)
+      return;
+
+    inorderTraversal(root->left);
+    cout<<root->data<<" ";
+    inorderTraversal(root->right);
+}
+//a=1,b=2,c=3,d=4,e=5,f=6,g=7
+int main(){
+  string s = "ab+ef*g*-" ;
+  //cin>>s;
+  struct node* result = expression(s);
+  inorderTraversal(result);
+  cout<<endl;
+  return 0;
+}
