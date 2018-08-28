49c6215c7270783a6138a93e2b5a810a1ac11d83
mittalprince <princemittal766@gmail.com>
2018-08-28 22:41:15 +0530

    Depth First Search

--- c++/graph/DFS.cpp
+++ c++/graph/DFS.cpp
@@ -21,8 +21,8 @@ void add(graph a[], int u, int v){
 	a[u].l.push_back(v);
 }
 
-
-void DFS(graph a[], int node){
+ 
+void DFS(graph a[]	, int node){
 
 	a[node].visited = true;
 	cout<<a[node].value<<endl;
@@ -38,17 +38,20 @@ int main(){
 
 	graph a[4];
 
-	assign(a,0,10);
-	assign(a,1,20);
-	assign(a,2,30);
-	assign(a,3,40);
+	assign(a,0,0);
+	assign(a,1,1);
+	assign(a,2,2);
+	assign(a,3,3);
 
 	add(a,0,1);
 	add(a,1,2);
 	add(a,1,3);
 	add(a,2,3);
 
-	DFS(a, 0);
+	for(int i=0;i<4;i++){
+		if(!a[i].visited)
+			DFS(a, i);
+	}
 
 	return 0;
 }
