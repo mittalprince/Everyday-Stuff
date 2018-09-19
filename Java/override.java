aba8a4c36d1fd6d7d442d242af9057c4a9abc065
mittalprince <princemittal766@gmail.com>
2018-09-19 22:29:39 +0530

    Overide in Java

---
+++ Java/override.java
@@ -0,0 +1,30 @@
+class Parent{
+	void show(){
+		 System.out.println("Parent's show()"); 
+	}
+}
+
+class Child extends Parent{
+	void show(){
+		 System.out.println("Child's show()"); 
+	}
+}
+
+class Main 
+{ 
+    public static void main(String[] args) 
+    { 
+        // If a Parent type reference refers 
+        // to a Parent object, then Parent's 
+        // show is called 
+        Parent obj1 = new Parent(); 
+        obj1.show(); 
+  
+        // If a Parent type reference refers 
+        // to a Child object Child's show() 
+        // is called. This is called RUN TIME 
+        // POLYMORPHISM. 
+        Parent obj2 = new Child(); 
+        obj2.show(); 
+    } 
+} 
