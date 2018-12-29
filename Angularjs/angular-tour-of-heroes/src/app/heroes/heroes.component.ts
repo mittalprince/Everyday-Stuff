744c9c307aa1105982cba573593360967ef77763
mittalprince <princemittal766@gmail.com>
2018-12-29 23:34:04 +0530

    Inject Services

--- Angularjs/angular-tour-of-heroes/src/app/heroes/heroes.component.ts
+++ Angularjs/angular-tour-of-heroes/src/app/heroes/heroes.component.ts
@@ -8,7 +8,7 @@ import { HeroService } from '../hero.service';
   styleUrls: ['./heroes.component.css']
 })
 export class HeroesComponent implements OnInit {
-  heroes = Hero[];
+  heroes : Hero[];
   selectedHero : Hero;
   onSelect(hero: Hero): void{
   this.selectedHero = hero;
@@ -18,9 +18,10 @@ export class HeroesComponent implements OnInit {
 
   getHeroes(): void {
   this.heroes = this.heroService.getHeroes();
-}
+  }
 
   ngOnInit() {
+    this.getHeroes();
   }
 
 }
