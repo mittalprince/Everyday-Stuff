6abda92771bcae3b7f31574b5c9fbf82452ec91f
mittalprince <princemittal766@gmail.com>
2018-12-29 20:08:52 +0530

    Add Services

--- Angularjs/angular-tour-of-heroes/src/app/heroes/heroes.component.ts
+++ Angularjs/angular-tour-of-heroes/src/app/heroes/heroes.component.ts
@@ -1,6 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { Hero } from '../hero';
-import {HEROES} from '../mock-heroes';
+import { HeroService } from '../hero.service';
 
 @Component({
   selector: 'app-heroes',
@@ -8,13 +8,17 @@ import {HEROES} from '../mock-heroes';
   styleUrls: ['./heroes.component.css']
 })
 export class HeroesComponent implements OnInit {
-  heroes = HEROES;
+  heroes = Hero[];
   selectedHero : Hero;
   onSelect(hero: Hero): void{
   this.selectedHero = hero;
   }		
 
-  constructor() { }
+  constructor(private heroService: HeroService) { }
+
+  getHeroes(): void {
+  this.heroes = this.heroService.getHeroes();
+}
 
   ngOnInit() {
   }
