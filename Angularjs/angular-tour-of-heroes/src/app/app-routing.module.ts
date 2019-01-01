7cb4a3dbf6073b362eb181b7fcb41c8bdd83eb70
mittalprince <princemittal766@gmail.com>
2019-01-01 23:28:03 +0530

    Add dashboard component and router to it

--- Angularjs/angular-tour-of-heroes/src/app/app-routing.module.ts
+++ Angularjs/angular-tour-of-heroes/src/app/app-routing.module.ts
@@ -1,9 +1,13 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import {HeroesComponent} from './heroes/heroes.component';
+import {DashboardComponent} from './dashboard/dashboard.component';
 
 const routes: Routes = [
-	{path: 'heroes', component: HeroesComponent}
+	{ path: '', redirectTo: '/dashboard', pathMatch: 'full' },
+  	{ path: 'dashboard', component: DashboardComponent },
+  	{ path: 'heroes', component: HeroesComponent }
+	
 ];
 
 @NgModule({
