6ce3f46aff6f2ecc918437cf20f7e4db906ed21a
mittalprince <princemittal766@gmail.com>
2019-06-03 22:06:54 +0530

    Learning Lazy-Loading

---
+++ Angular/Lazy-Loading/testing-app/src/app/app-routing.module.ts
@@ -0,0 +1,23 @@
+import { NgModule } from '@angular/core';
+import { Routes, RouterModule } from '@angular/router';
+
+const routes: Routes = [
+  {
+    path:'customers',
+    loadChildren: () => import('./customers/customers.module').then(mod => mod.CustomersModule)
+  },{
+    path: 'orders',
+    loadChildren: () => import('./orders/orders.module').then(mod => mod.OrdersModule)
+  },
+  {
+    path: '',
+    redirectTo: '',
+    pathMatch: 'full'
+  }
+];
+
+@NgModule({
+  imports: [RouterModule.forRoot(routes)],
+  exports: [RouterModule]
+})
+export class AppRoutingModule { }
