5c829cc4a4f74e1bd02749a173a79012dd25954e
mittalprince <princemittal766@gmail.com>
2019-06-30 22:47:49 +0530

    Template Practice

--- Angular/Form-validation/src/app/app-routing.module.ts
+++ Angular/Form-validation/src/app/app-routing.module.ts
@@ -4,11 +4,14 @@ import { Routes, RouterModule } from '@angular/router';
 import { reactiveComponent } from './reactive/reactive.component'
 import { templateDrivenComponent } from './template-driven/template-driven.component'
 import { landingComponent } from './landing/landing.component'
+import { TemplateComponent } from './issue-template/template.component'
 
 const routes: Routes = [
+  {path: '', redirectTo: 'template', pathMatch: 'full'},
   {path:'', component:landingComponent},
   {path:'reactive', component: reactiveComponent},
-  {path:'template-driven', component: templateDrivenComponent}
+  {path:'template-driven', component: templateDrivenComponent},
+  {path: 'template', component: TemplateComponent}
 ];
 
 @NgModule({
@@ -20,5 +23,6 @@ export class AppRoutingModule { }
 export const routingComponent = [
   reactiveComponent,
   templateDrivenComponent,
-  landingComponent
+  landingComponent,
+  TemplateComponent
 ]
---
+++ Angular/Form-validation/src/app/issue-template/template.component.ts
@@ -0,0 +1,14 @@
+import { Component, OnInit } from '@angular/core'
+
+@Component({
+    selector: 'app-issue-template',
+    templateUrl: './template.component.html',
+    styleUrls: ['./template.component.css']
+})
+
+export class TemplateComponent implements OnInit{
+
+    constructor(){}
+
+    ngOnInit(){}
+}
