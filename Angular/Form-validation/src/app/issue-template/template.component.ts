c1663ecd2c7d925c893143da5e8f88ba9793faa9
mittalprince <princemittal766@gmail.com>
2019-07-01 23:25:05 +0530

    Change in css

--- Angular/Form-validation/src/app/issue-template/template.component.ts
+++ Angular/Form-validation/src/app/issue-template/template.component.ts
@@ -8,7 +8,46 @@ import { Component, OnInit } from '@angular/core'
 
 export class TemplateComponent implements OnInit{
 
+    toggleDetails: boolean = false
+    assign: Boolean = false
+
     constructor(){}
 
     ngOnInit(){}
+
+    toggle(){
+        if (this.toggleDetails == false) {
+            this.toggleDetails = true
+            this.cancelUpdateAssign()
+        } else {
+            return 0
+        }
+    }
+
+    closeDetails() {
+        if (this.toggleDetails == true) {
+            this.toggleDetails = false
+        } else {
+            return 0
+        }
+    }
+
+    cancelUpdateAssign(){
+        if(this.assign == true){
+            this.assign = false
+        }
+        else{
+            return 0
+        }
+    }
+
+    updateAssign(){
+        if(this.assign == false){
+            this.assign = true
+            this.closeDetails()
+        }
+        else{
+            return 0
+        }
+    }
 }
