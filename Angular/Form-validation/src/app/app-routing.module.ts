f3a8ecdbd5155532ca53cf049a430527c29c6510
mittalprince <princemittal766@gmail.com>
2019-07-03 08:10:44 +0530

    Shimmer Effect

--- Angular/Form-validation/src/app/app-routing.module.ts
+++ Angular/Form-validation/src/app/app-routing.module.ts
@@ -5,13 +5,15 @@ import { reactiveComponent } from './reactive/reactive.component'
 import { templateDrivenComponent } from './template-driven/template-driven.component'
 import { landingComponent } from './landing/landing.component'
 import { TemplateComponent } from './issue-template/template.component'
+import { ShimmerTemplateComponent } from './issue-template/shimmering-template/shimmering-template.component'
 
 const routes: Routes = [
-  {path: '', redirectTo: 'template', pathMatch: 'full'},
+  {path: '', redirectTo: 'shimmer-template', pathMatch: 'full'},
   {path:'', component:landingComponent},
   {path:'reactive', component: reactiveComponent},
   {path:'template-driven', component: templateDrivenComponent},
-  {path: 'template', component: TemplateComponent}
+  {path: 'template', component: TemplateComponent},
+  {path: 'shimmer-template', component: ShimmerTemplateComponent}
 ];
 
 @NgModule({
@@ -24,5 +26,6 @@ export const routingComponent = [
   reactiveComponent,
   templateDrivenComponent,
   landingComponent,
-  TemplateComponent
+  TemplateComponent,
+  ShimmerTemplateComponent
 ]
---
+++ Angular/Form-validation/src/app/issue-template/shimmering-template/shimmering-template.component.ts
@@ -0,0 +1,77 @@
+import { Component, OnInit } from '@angular/core'
+
+@Component({
+    selector: 'app-shimmering-template',
+    templateUrl: './shimmering-template.component.html',
+    styleUrls: ['./shimmering-template.component.css']
+})
+
+export class ShimmerTemplateComponent implements OnInit{
+
+    toggleDetails: boolean = false
+    assign: Boolean = false
+
+    background: any = [
+        '#39f', '#1e87f0', '#1e87f0', '#f0506e', '#5af050', '#32d296', '#d23250', '#d2a8329c', '#512DA8'
+    ]
+    constructor(){}
+
+    ngOnInit(){
+        this.setImage()
+    }
+
+    toggle(){
+        if (this.toggleDetails == false) {
+            this.toggleDetails = true
+            this.cancelUpdateAssign()
+        } else {
+            return 0
+        }
+    }
+
+    closeDetails() {
+        if (this.toggleDetails == true) {
+            this.toggleDetails = false
+        } else {
+            return 0
+        }
+    }
+
+    cancelUpdateAssign(){
+        if(this.assign == true){
+            this.assign = false
+        }
+        else{
+            return 0
+        }
+    }
+
+    updateAssign(){
+        if(this.assign == false){
+            this.assign = true
+            this.closeDetails()
+        }
+        else{
+            return 0
+        }
+    }
+
+    setImage(){
+        // if(document.getElementById('profile')){
+        //     document.getElementById('profile').innerHTML = 'PM'
+        // }
+        // document.getElementById('profile').innerHTML = 'PM'
+
+        return 'PM'
+    }
+
+    setBackground(){
+        let randomSVG = this.background[Math.floor(Math.random() * this.background.length)];
+        console.log(randomSVG)
+        let style = {
+            'background': `${randomSVG}`
+        }
+
+        return style
+    }
+}
