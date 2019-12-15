8036690403cb9d4031b204da4be6152af3575096
mittalprince <princemittal766@gmail.com>
2019-12-15 19:30:49 +0530

    Front end Practice

--- Golang/hello.go
+++ Golang/hello.go
@@ -9,4 +9,14 @@ func main(){
 	fmt.Println("Welcome to the playground!")
 
 	fmt.Println("The time is", time.Now())
+
+	fmt.Println(len("Hello World"))
+  fmt.Println("Hello World"[1])
+  fmt.Println("Hello " + "World")
+
+  fmt.Println(true && true)
+  fmt.Println(true && false)
+  fmt.Println(true || true)
+  fmt.Println(true || false)
+  fmt.Println(!true)
 }
